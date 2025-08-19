f29521e31ed0dbe887bcc84fe13fc715fe2f1d20
Josue Rahn <rahnjosue83@gmail.com>
2025-08-18 22:52:30 -0300

    estetica registro-login mejorada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Perfil')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-light d-flex flex-column justify-content-center align-items-center min-vh-100">
     <main class="container flex-grow-1 d-flex flex-column justify-content-center align-items-center">
